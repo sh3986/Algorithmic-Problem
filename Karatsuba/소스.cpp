4033be5594a7ec67012c178a4dc383f1043e9fdc
sh3986 <58520304+sh3986@users.noreply.github.com>
2019-12-11 21:35:44 +0900

    Update 소스.cpp

--- Karatsuba/소스.cpp
+++ Karatsuba/소스.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 template<typename T1, typename T2>
-constexpr auto MIN(T1 x, T2 y) { return x > y ? x : y; }
+constexpr auto MIN(T1 x, T2 y) { return x < y ? x : y; }
 
 using namespace std;
 
@@ -13,17 +13,18 @@ void subFrom(vector<int>& a, const vector<int>& b);
 
 vector<int> karatsuba(const vector<int>& a, const vector<int>& b);
 int main() {
-	vector<int> a{ 1,2,3,4 };
-	vector<int> b{ 4,5,2,1 };
-	vector<int> s = multiply(a, b);
+	vector<int> a{ 1,2,3,4,3 };
+	vector<int> b{ 3,2 };
+	vector<int> s = karatsuba(a, b);
 
+	
 	return 0;
 }
 
 void normalize(vector<int>& a) {
 	a.push_back(0);
-	
-	for (int i = 0; i < a.size()-1; ++i) {
+
+	for (int i = 0; i < a.size() - 1; ++i) {
 		if (a[i] < 0) {
 			int borrow = (-1 * a[i] + 9) / 10;
 			a[i + 1] -= borrow;
@@ -35,7 +36,7 @@ void normalize(vector<int>& a) {
 		}
 	}
 
-	while(a.size() > 0 && a.back() == 0)
+	while (a.size() > 0 && a.back() == 0)
 		a.pop_back();
 }
 
@@ -55,12 +56,12 @@ void addTo(vector<int>& a, const vector<int>& b, int k) {
 	vector<int> c(b.size() + k, 0);
 	for (int i = 0; i < b.size(); ++i)
 		c[i + k] = b[i];
-	
+
 	int aLength = a.size();
 	int cLength = c.size();
 
-	for (int i = 0; i < cLength - aLength; ++i) 
-		a.push_back(0);  
+	for (int i = 0; i < cLength - aLength; ++i)
+		a.push_back(0);
 
 	for (int i = 0; i < c.size(); ++i) {
 		a[i] += c[i];
@@ -84,12 +85,36 @@ vector<int> karatsuba(const vector<int>& a, const vector<int>& b) {
 
 	if (an == 0 || bn == 0) return vector<int>();
 
-	if (an <= 50) return multiply(a, b);
+	if (an <= 2) return multiply(a, b);
 
 	int half = an / 2;
 	vector<int> a0(a.begin(), a.begin() + half);
 	vector<int> a1(a.begin() + half, a.end());
+	vector<int> b0(b.begin(), b.begin() + MIN(half, b.size()));
+	
+	vector<int> b1;
+	if(half< b.size()){
+		b1 = vector<int>(b.begin() + half, b.end());
+	}
+	else {
+		b1 = vector<int>(1,1);
+	}
 
-	return vector<int>();
-	//vector<int> b0(b.begin(), min)
+	vector<int> z0 = karatsuba(a1, b1);
+	vector<int> z2 = karatsuba(a0, b0);
+
+	addTo(a1, a0, 0);
+	addTo(b1, b0, 0);
+
+	vector<int> z1 = karatsuba(a1, b1);
+	subFrom(z1, z0);
+	subFrom(z1, z2);
+
+	vector<int> ret(1);
+	
+	addTo(ret, z2, 0);
+	addTo(ret, z1, half);
+	addTo(ret, z0, half + half);
+
+	return ret;
 }
